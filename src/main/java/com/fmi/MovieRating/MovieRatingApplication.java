67cf938f4e540d5d994f4015cf4def178b73c08b
Kristian Simov <krisi.simov@gmail.com>
2022-05-13 18:27:09 +0300

    initial commit

---
+++ src/main/java/com/fmi/MovieRating/MovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.MovieRating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmi/MovieRating/MovieRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.MovieRating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
