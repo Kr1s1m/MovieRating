61350d16c2595addb43e5a36e17e96f763bd05a1
Ana Petrova <petrovaan@petrovaan-a01.vmware.com>
2022-05-14 14:36:52 +0300

    Test

--- src/main/java/com/fmi/MovieRating/MovieRatingApplication.java
+++ src/main/java/com/fmi/MovieRating/MovieRatingApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieRatingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(MovieRatingApplication.class, args);
 	}
+
+
 
 }
