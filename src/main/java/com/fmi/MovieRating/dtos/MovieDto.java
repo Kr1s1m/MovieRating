26f950defc9b857dcdf6a5e9ef9ed4e48f113765
kkgergov <zerovelocity@abv.bg>
2022-06-07 17:24:47 +0300

    added default constructor to dtos

--- src/main/java/com/fmi/MovieRating/dtos/MovieDto.java
+++ src/main/java/com/fmi/MovieRating/dtos/MovieDto.java
@@ -2,12 +2,21 @@ package com.fmi.MovieRating.dtos;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MovieDto {
     private Integer id;
     private String title;
     private Short year;
     private String description;
+
+    public MovieDto() {
+        id = 0;
+        title = "";
+        year = 0;
+        description = "";
+    }
 
     //constructor
     public MovieDto(String title, Short year, String description) {
