ce0c62127a1013e72ada6cb53814469c87559a5b
kkgergov <zerovelocity@abv.bg>
2022-06-22 12:39:32 +0300

    made the reviewExistsByAccIdAndMovieId query mutch faster and cleaner, beginning to implement the vote model

---
+++ src/main/java/com/fmi/MovieRating/models/Vote.java
@@ -0,0 +1,30 @@
+/*
+package com.fmi.MovieRating.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
+*/
